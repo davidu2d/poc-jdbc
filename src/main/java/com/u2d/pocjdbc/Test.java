b97c9e3a61cbe89538ac5ecb85c4fe4291fdb324
davidu2d <u2dtecnologia@gmail.com>
2025-04-05 23:05:47 -0300

    add test

--- src/main/java/com/u2d/pocjdbc/Test.java
+++ src/main/java/com/u2d/pocjdbc/Test.java
@@ -4,11 +4,16 @@ import com.u2d.pocjdbc.dao.ClienteDAO;
 import com.u2d.pocjdbc.dao.impl.ClienteDAOImpl;
 import com.u2d.pocjdbc.model.Cliente;
 
+import java.io.UnsupportedEncodingException;
 import java.sql.SQLException;
 import java.util.List;
 
 public class Test {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException, UnsupportedEncodingException {
+        StringBuffer b = new StringBuffer("teste");
+        byte[] byt = String.valueOf(b).getBytes();
+        byt = new String(byt, "ISO-8859-1").getBytes("UTF-8");
+        b = new StringBuffer()
         ClienteDAO dao = new ClienteDAOImpl();
 
         Cliente cliente = Cliente.builder()
